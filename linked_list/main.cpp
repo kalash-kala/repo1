838d4b18641cd20dbcf0331988b7e8423f7deb11
kalash-kala <kalashkala@gmail.com>
2021-04-24 19:57:06 +0530

    Initial Commit

---
+++ linked_list/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  linked_list
+//
+//  Created by kalash kala on 24/04/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
