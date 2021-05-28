60f56768c5ea49d307e9f0da91bd03f3762babe8
kalashkala <kalashkala@gmail.com>
2021-05-28 10:17:57 +0530

    Initial Commit

---
+++ Queue_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Queue_cpp
+//
+//  Created by kalash kala on 28/05/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
